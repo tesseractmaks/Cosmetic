6a237e40856293eb521605c4318226880539db27
tesseract <tesseractmaks@gmail.com>
2023-12-07 17:32:05 +0800

    Add test data

--- cosmetic_app/cosmetic_app/db/db_init.py
+++ cosmetic_app/cosmetic_app/db/db_init.py
@@ -1,5 +1,7 @@
 import asyncpg
 from sqlalchemy import inspect, insert
+from faker import Faker
+import random
 
 from cosmetic_app.models import User, Product, Profile, Category, Brand
 from .base_class import Base
@@ -26,14 +28,77 @@ async def init_db():
         )
         if not tables:
             await conn.run_sync(Base.metadata.create_all)
-        await add_test_data(conn)
 
-async def add_test_data(conn):
-    values_data = {
-        "username": "one_user",
-        "permissions": [1, 2],
-        "is_active": True,
-        "groups": [2, 3],
-    }
-    await conn.execute(insert(UserModel).values(values_data))
+    await add_test_user()
+    await add_test_profile()
+    await add_test_product()
+    await add_test_category()
+    await add_test_brand()
+
+
+from faker.providers.person.ru_RU import Provider
+fake = Faker("ru_RU")
+
+
+async def add_test_user():
+    async with db_helper.engine.connect() as conn:
+        for i in range(1, 10):
+            values_data = {
+                "email": fake.email(),
+                "password": "string",
+                "is_active": True,
+                "profile": i
+            }
+            await conn.execute(insert(User).values(values_data))
+            await conn.flush()
+            await conn.commit()
+
+
+async def add_test_profile():
+    async with db_helper.engine.connect() as conn:
+        for i in range(10):
+            values_data = {
+                "nickname": fake.email(),
+                "phone": fake.phone_number(),
+                "photo": "string",
+                "first_name": Provider.first_name,
+                "last_name": Provider.last_name,
+                "user_id": i,
+                "user": User
+            }
+            await conn.execute(insert(Profile).values(values_data))
+
+
+async def add_test_product():
+    async with db_helper.engine.connect() as conn:
+        for _ in range(20):
+            values_data = {
+                "title": random.choice(["Eyeliner", "Lipstick", "Blush", "Mascara", "Eyeshadow"]),
+                "article_number": f"#{random.randint(200, 300)}",
+                "price": random.randint(40, 210),
+                "image": "string",
+                "brand_id": random.randint(1, 3),
+                "brand": Brand,
+                "category_id": random.randint(1, 3),
+                "categories": Category,
+            }
+            await conn.execute(insert(Product).values(values_data))
+
+async def add_test_category():
+    async with db_helper.engine.connect() as conn:
+        for i in ["Green", "Blue", "White"]:
+            values_data = {
+                "title": i,
+            }
+            await conn.execute(insert(Category).values(values_data))
+
+
+async def add_test_brand():
+    async with db_helper.engine.connect() as conn:
+        for i in ["RevitaYouth", "SereneSilk", "PureHarmony"]:
+            values_data = {
+                "title": i,
+            }
+            await conn.execute(insert(Brand).values(values_data))
+
 
